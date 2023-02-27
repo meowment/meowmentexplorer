cbcc7eaf81d9a1a1d6639ff9cb0d55e959226fae
meowment <robbycahr@gmail.com>
2023-02-27 15:47:25 +0700

    Update themeConfig.js

--- themeConfig.js
+++ themeConfig.js
@@ -9,7 +9,7 @@ export const $themeBreakpoints = {}
 // APP CONFIG
 export const $themeConfig = {
   app: {
-    appName: 'MEOWMENT', // Will update name in navigation menu (Branding)
+    appName: 'MEOWMENT EXPLORER', // Will update name in navigation menu (Branding)
     // eslint-disable-next-line global-require
     appLogoImage: '/logox.svg', // Will update logo in navigation menu (Branding)
   },
