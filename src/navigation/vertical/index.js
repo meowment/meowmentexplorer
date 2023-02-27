b8b056a4ea69fff0cf65b45578909dcce337c8d9
meowment <robbycahr@gmail.com>
2023-02-27 14:57:26 +0700

    Update index.js

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -19,7 +19,7 @@ function processMenu() {
       title: 'blockchains',
       children: blockchains,
       tag: `${blockchains.length}`,
-      icon: '/logo.svg',
+      icon: '/logox.svg',
     })
   }
   chainMenus.push({ header: 'LINKS' })
