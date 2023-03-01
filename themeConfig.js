1cc985cb7139d6084ab7f5a28c09d318873ceafb
meowment <robbycahr@gmail.com>
2023-03-01 21:27:59 +0700

    Update themeConfig.js

--- themeConfig.js
+++ themeConfig.js
@@ -26,7 +26,7 @@ export const $themeConfig = {
     navbar: {
       // ? For horizontal menu, navbar type will work for navMenu type
       type: 'sticky', // static , sticky , floating, hidden
-      backgroundColor: 'primary', // BS color options [primary, success, etc]
+      backgroundColor: 'dark', // BS color options [primary, success, etc]
     },
     footer: {
       type: 'static', // static, sticky, hidden
